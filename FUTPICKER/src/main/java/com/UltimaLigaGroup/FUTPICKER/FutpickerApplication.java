281e331100d22e68853d758660838a45a889e10f
JET-VORTEX\isilv <icsf2442@gmail.com>
2026-07-08 19:37:06 +0100

    First commit, barebones structure

---
+++ FUTPICKER/src/main/java/com/UltimaLigaGroup/FUTPICKER/FutpickerApplication.java
@@ -0,0 +1,13 @@
+package com.UltimaLigaGroup.FUTPICKER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutpickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutpickerApplication.class, args);
+	}
+
+}
---
+++ FUTPICKER/src/test/java/com/UltimaLigaGroup/FUTPICKER/FutpickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UltimaLigaGroup.FUTPICKER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutpickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
